e9f6f43db6d38af2142097842f3f38e5db1f7045
silverstar03 <eunbyoul0421@naver.com>
2021-06-02 20:46:17 +0900

    의존 배열(deps)활용하기

---
+++ src/use-effect-demo/use-effect-intro.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react'
+import ReactDOM from "react-dom"
+function UseEffectDemo(props) {
+    const [state, setState] = useState("")
+// 의존 배열이 전달되지 않았으므로 매 render 시점 이후 전달할 콜백 함수가 실행됨
+    useEffect(() => {
+        console.log("useEffect")
+    })
+    console.log("render")
+    return (
+        <div>
+            <input type="text" value={state}
+                   onChange={(e) => {
+                       setState(e.target.value)
+                   }} />
+        </div>
+    )
+}
+function Counter() {
+    const [count, setCount] = useState(0);
+// DOM 조작은 effect 콜백 함수에서 수행
+    useEffect(() => {
+        // 매 render 이후 제목 내용 바꾸기
+        document.title = `You clicked ${count} times`;
+        //의존 배열에 count를 넣어서 사용하면 개선 가능
+    }, [count]);
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>Click me</button>
+        </div>
+    );
+}
+ReactDOM.render((
+    <div>
+        <UseEffectDemo />
+        <Counter />
+    </div>
+), document.getElementById("root"))
