2f138862a5352752dabb10a076151101135c462a
silverstar03 <eunbyoul0421@naver.com>
2021-06-28 20:49:40 +0900

    use-context를 이용한 테마 바꾸기 실습

---
+++ src/use-context-demo/use-context-using-theme-context.js
@@ -0,0 +1,36 @@
+import React, { useState, useContext } from "react"
+import ReactDOM from "react-dom"
+import ThemeContext from "./contexts/ThemeContext"
+
+const ThemeConsumeComponent = (props) => {
+    // Context를 통해서 전달받은 값과 세터 대입
+    const { theme, setTheme } = useContext(ThemeContext)
+
+    const themeStyle = theme === 'light' ?
+        { color: 'black', backgroundColor: 'white', padding: '20px' } :
+        { color: 'white', backgroundColor: 'black', padding: '20px' }
+
+    return (
+        <div style={themeStyle}>
+            <h1>ThemeConsumeComponent { theme === 'light' ? '🌞' : '🌙' }</h1>
+            <p>currentTheme : { theme }</p>
+            {/*setTheme는 전역에 접근*/}
+            <button onClick={() => { setTheme('light') }}>light</button>
+            <button onClick={() => { setTheme('dark') }}>dark</button>
+        </div>
+    )
+}
+
+function App() {
+    // Provider를 통해 전달할 값과 세터 생성
+    const [ theme, setTheme ] = useState('light')
+
+    return (
+        <ThemeContext.Provider value={ { theme, setTheme } }>
+            {/* useState 훅을 이용하여 반환받은 값과 세터 함수를 Provider를 통해 Context 값으로 전달 */}
+            <ThemeConsumeComponent />
+        </ThemeContext.Provider>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
